d6cb28e02f448b4374cbbd450192e828ea8e83cd
valentinkhoo <valentin_khoo@outlook.com>
2022-03-15 21:41:06 +0800

    Fixed updating of number of rents unpaid
    now auto update number of months rental unpaid when new rental is added

--- src/views/MyRentals.vue
+++ src/views/MyRentals.vue
@@ -318,32 +318,35 @@ export default {
     };
   },
   async mounted() {
-    const auth = getAuth();
-    const userEmail = auth.currentUser.email;
-    const ref = doc(db, "Rentals", userEmail);
-    const docSnap = await getDoc(ref);
-    const rentals = docSnap.data().rentals;
-
-    for (let rental of rentals) {
-      console.log(rental);
-      for (let tenant of rental.tenants) {
-        console.log(tenant);
-        var nextPaymentDate = moment(tenant.nextPaymentDate);
-
-        if (nextPaymentDate.isBefore(moment())) {
-          console.log("owe money");
-          tenant.numberOfMonthsRentalUnpaid += 1;
-          tenant.nextPaymentDate = this.addMonths(nextPaymentDate, 1);
-        }
-      }
-    }
-    console.log(rentals);
-    await updateDoc(ref, { rentals: rentals });
+    await this.updateUnpaid();
   },
 
   methods: {
     addMonths(date, m) {
       return moment(date).add(m, "months").format("YYYY-MM-DD");
+    },
+
+    async updateUnpaid() {
+      const auth = getAuth();
+      const userEmail = auth.currentUser.email;
+      const ref = doc(db, "Rentals", userEmail);
+      const docSnap = await getDoc(ref);
+      const rentals = docSnap.data().rentals;
+
+      for (let rental of rentals) {
+        console.log(rental);
+        for (let tenant of rental.tenants) {
+          console.log(tenant);
+          while (moment(tenant.nextPaymentDate).isBefore(moment())) {
+            console.log("owe money");
+            tenant.numberOfMonthsRentalUnpaid += 1;
+            tenant.nextPaymentDate = this.addMonths(tenant.nextPaymentDate, 1);
+          }
+        }
+      }
+      console.log(rentals);
+      await updateDoc(ref, { rentals: rentals });
+      console.log("updated number of months rental unpaid!")
     },
 
     async saveRental() {
@@ -374,7 +377,7 @@ export default {
         unitNumber: this.unitNumber,
         purchasePrice: this.purchasePrice,
         longtitude: long,
-        latitude: lat, 
+        latitude: lat,
 
         tenants: [
           {
@@ -400,6 +403,7 @@ export default {
       }
 
       document.getElementById("addRentalForm").reset();
+      this.updateUnpaid();
     },
   },
 };
