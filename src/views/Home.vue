0dcba01b7854220df4cd5ea45c50e0cc7e1341d9
saxyrinchen <saxyrinchen.99@gmail.com>
2022-04-02 09:24:46 +0800

    Update Home.vue
    charts need to work!!!!

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -77,11 +77,19 @@
         </div>
       </div>
     </div>
-    <h4>revenue against time</h4>
-    <line-chart :data="revenueAgainstTimeData">TIMESERIES</line-chart>
+    <h4>revenue against time (default)</h4>
     <br>
-    <h4>cumulative revenue against time</h4>
-    <line-chart :data="cumulativeRevenueAgainstTimeData">TIMESERIES</line-chart>
+    <button v-on:click = "foobarbarblacksheep"> touch me </button>
+    <br>
+    <line-chart :data="revenueAgainstTimeDataDefault" v-if ="foo == true" >TIMESERIES</line-chart>
+    <line-chart :data="revenueExpensesAgainstTimeDataByMonth" 
+      :colors="['#b00', '#666']"
+      xmin="findLowerBound" xmax="findUpperBound" 
+      v-if ="foo == false"
+      ytitle="in Singapore Dollars ($SGD)"></line-chart>
+
+    <!-- <h4>cumulative revenue against time</h4>
+    <line-chart :data="cumulativeRevenueAgainstTimeData">TIMESERIES</line-chart> -->
   </div>
 </template>
 
@@ -224,6 +232,7 @@ export default {
       rentals: [],
       filterStartDate: "",
       filterEndDate: "",
+      foo: true,
     };
   },
 
@@ -297,6 +306,7 @@ export default {
               (moment(tenantRevenues.paymentDate).isSameOrAfter(moment(this.filterStartDate)) &&
               moment(tenantRevenues.paymentDate).isSameOrBefore(moment(this.filterEndDate)))
             ) {
+              console.log(tenantRevenues.paymentDate);
               let tenantRevenuesPaymentAmount = JSON.parse(
                 JSON.stringify(tenantRevenues.paymentAmount)
               );
@@ -317,71 +327,236 @@ export default {
       console.log(result);
       return result;
     },
-
-    revenueAgainstTimeData() {
+    revenueAgainstTimeDataDefault() { // if the day dont have collect, put 0
       // {time, }
       var result = {};
       // initalise all dates to 0 payment amount
       for (let rental of this.rentals) {
         for (let tenant of rental.tenants) {
           for (let tenantRevenues of tenant.revenues) {
+            // set all dates to 0
             let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
             result[tenantRevenuePaymentDate] = 0;
           }
         }
       }
-      console.log("initalised result:", result);
-      for (let rental of this.rentals) {
-        for (let tenant of rental.tenants) {
-          for (let tenantRevenues of tenant.revenues) {
-            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
-            result[tenantRevenuePaymentDate] += tenantRevenues.paymentAmount;
-          }
-        }
-      }
-      console.log("unsorted", result);
-      return result;      
-    },
-
-    cumulativeRevenueAgainstTimeData() {
-      // {time, }
-      var result = {};
-      // initalise all dates to 0 payment amount
-      for (let rental of this.rentals) {
-        for (let tenant of rental.tenants) {
-          for (let tenantRevenues of tenant.revenues) {
-            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
-            result[tenantRevenuePaymentDate] = 0;
-          }
-        }
-      }
-      console.log("initalised result:", result);
-      for (let rental of this.rentals) {
-        for (let tenant of rental.tenants) {
-          for (let tenantRevenues of tenant.revenues) {
-            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
-            result[tenantRevenuePaymentDate] += tenantRevenues.paymentAmount;
-          }
-        }
-      }
-      console.log("unsorted", result);
-
-      let sortedResult = Object.keys(result)
+      result = Object.keys(result)
         .sort()
         .reduce(function (acc, key) {
           acc[key] = result[key];
           return acc;
         }, {});
-      
-      var cumulativeRevenue = 0;
-      for (const [key, value] of Object.entries(sortedResult)) {
-        console.log(`${key}: ${value}`);
-        cumulativeRevenue += value;
-        sortedResult[key] = cumulativeRevenue;
+
+      // result.map((x) => moment(x).format("YYYY-MM"))
+
+      console.log("sorted initialised result:", result);
+
+      var startDate = moment(Object.keys(result)[0]);
+      var endDate = moment(Object.keys(result)[Object.keys(result).length - 1]);
+      var currentDate = startDate;
+      while (currentDate.isBefore(endDate)) {
+        var nextDate = startDate.add(1, 'days').format("YYYY-MM-DD");
+        result[nextDate] = 0;
       }
-      console.log(sortedResult);
-      return sortedResult;      
+      
+      for (let rental of this.rentals) {
+        for (let tenant of rental.tenants) {
+          for (let tenantRevenues of tenant.revenues) {
+            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
+            result[tenantRevenuePaymentDate] += tenantRevenues.paymentAmount;
+          }
+        }
+      }
+      console.log("unsorted", result);
+      return result;   
     },
+
+    revenueExpensesAgainstTimeDataByMonth() { 
+      
+      // REVENUES GRAPH 
+      // need settle the filtering for rentals
+      // for (let rental of this.rentals) {
+      //   for (let tenant of rental.tenants) {
+      //     for (let tenantRevenues of tenant.revenues) {
+      //       if (
+      //         this.filterStartDate &&
+      //         this.filterEndDate &&
+      //         (moment(tenantRevenues.paymentDate).isSameOrAfter(moment(this.filterStartDate)) &&
+      //         moment(tenantRevenues.paymentDate).isSameOrBefore(moment(this.filterEndDate)))
+      //       ) {
+      //         console.log(tenantRevenues.paymentDate);
+      //         let tenantRevenuesPaymentAmount = JSON.parse(
+      //           JSON.stringify(tenantRevenues.paymentAmount)
+      //         );
+      //         if (tenantRevenuesPaymentAmount) {
+      //           result[rental.address] += parseInt(tenantRevenuesPaymentAmount);
+      //         }
+      //       } else if (this.filterStartDate && this.filterEndDate) {
+      //         continue;
+      //       } else {
+      //         let tenantRevenuesPaymentAmount = JSON.parse(
+      //           JSON.stringify(tenantRevenues.paymentAmount)
+      //         );
+      //         result[rental.address] += parseInt(tenantRevenuesPaymentAmount);
+      //       }
+      //     }
+      //   }
+      // }
+      var revenueTemp = {};
+      // initalise all dates to 0 payment amount
+      for (let rental of this.rentals) {
+        for (let tenant of rental.tenants) {
+          for (let tenantRevenues of tenant.revenues) {
+            // set all dates to 0
+            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
+            revenueTemp[tenantRevenuePaymentDate] = 0;
+          }
+        }
+      }
+      revenueTemp = Object.keys(revenueTemp)
+        .sort()
+        .reduce(function (acc, key) {
+          acc[key] = revenueTemp[key];
+          return acc;
+        }, {});
+
+      // result.map((x) => moment(x).format("YYYY-MM"))
+
+      console.log("sorted initialised result:", revenueTemp);
+
+      var startDate = moment(Object.keys(revenueTemp)[0]);
+      var endDate = moment(Object.keys(revenueTemp)[Object.keys(revenueTemp).length - 1]);
+      var currentDate = startDate;
+      while (currentDate.isBefore(endDate)) {
+        var nextDate = startDate.add(1, 'days').format("YYYY-MM-DD");
+        revenueTemp[nextDate] = 0;
+      }
+      
+      for (let rental of this.rentals) {
+        for (let tenant of rental.tenants) {
+          for (let tenantRevenues of tenant.revenues) {
+            let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
+            revenueTemp[tenantRevenuePaymentDate] += tenantRevenues.paymentAmount;
+          }
+        }
+      }
+
+      let revenueFinal = {}; 
+      for (const [key] of Object.entries(revenueTemp)) {
+        revenueFinal[moment(key).format('YYYY-MM')] = 0; 
+      }
+
+      for (const [key, value] of Object.entries(revenueTemp)) {
+        revenueFinal[moment(key).format('YYYY-MM')] += value; 
+      }
+      console.log("final revenues",revenueFinal);
+
+      // EXPENSES GRAPH
+      let expenses = this.expenses;
+      if (this.filterStartDate && this.filterEndDate) {
+        console.log("hi");
+        expenses = this.expenses.filter((exp) =>
+          moment(exp.expenseDate).isSameOrAfter(moment(this.filterStartDate)) && 
+          moment(exp.expenseDate).isSameOrBefore(moment(this.filterEndDate))
+        );
+      }
+      var expensesTemp = {};
+      // initalise all dates to 0 payment amount
+      for (let expense of expenses) {
+        expensesTemp[expense.expenseDate] = 0;
+      }
+
+      expensesTemp = Object.keys(expensesTemp)
+        .sort()
+        .reduce(function (acc, key) {
+          acc[key] = expensesTemp[key];
+          return acc;
+        }, {});
+
+      // result.map((x) => moment(x).format("YYYY-MM"))
+
+      // console.log("sorted initialised result:", revenueTemp);
+      console.log(expensesTemp);
+      var startDate1 = moment(Object.keys(expensesTemp)[0]);
+      var endDate1 = moment(Object.keys(expensesTemp)[Object.keys(expensesTemp).length - 1]);
+      var currentDate1 = startDate1;
+      while (currentDate1.isBefore(endDate1)) {
+        var nextDate1 = startDate1.add(1, 'days').format("YYYY-MM-DD");
+        expensesTemp[nextDate1] = 0;
+      }
+      
+      for (let expense of this.expenses) {
+        expensesTemp[expense.expenseDate]  += expense.expenseCost;
+      }
+      let expensesFinal = {}; 
+      for (const [key] of Object.entries(expensesTemp)) {
+        expensesFinal[moment(key).format('YYYY-MM')] = 0; 
+      }
+
+      for (const [key, value] of Object.entries(expensesTemp)) {
+        expensesFinal[moment(key).format('YYYY-MM')] += value; 
+      }
+      console.log("final expenses",expensesFinal);
+
+      var data = [{name: 'Revenue', data: revenueFinal},
+                  {name: 'Expenses', data: expensesFinal},
+                  ];
+      return data;      
+    },
+
+    findLowerBound() {
+      if (!this.filterStartDate) {
+        return moment().subtract(1, 'year').format("YYYY-MM"); // "2021-04"
+      }
+      return this.filterStartDate;
+    },
+
+    findUpperBound() {
+      if (!this.filterEndDate) {
+        return moment().format("YYYY-MM");
+      }
+      return this.filterEndDate;
+    }
+
+    // cumulativeRevenueAgainstTimeData() {
+    //   // {time, }
+    //   var result = {};
+    //   // initalise all dates to 0 payment amount
+    //   for (let rental of this.rentals) {
+    //     for (let tenant of rental.tenants) {
+    //       for (let tenantRevenues of tenant.revenues) {
+    //         let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
+    //         result[tenantRevenuePaymentDate] = 0;
+    //       }
+    //     }
+    //   }
+    //   console.log("initalised result:", result);
+    //   for (let rental of this.rentals) {
+    //     for (let tenant of rental.tenants) {
+    //       for (let tenantRevenues of tenant.revenues) {
+    //         let tenantRevenuePaymentDate = tenantRevenues.paymentDate;
+    //         result[tenantRevenuePaymentDate] += tenantRevenues.paymentAmount;
+    //       }
+    //     }
+    //   }
+    //   console.log("unsorted", result);
+
+    //   let sortedResult = Object.keys(result)
+    //     .sort()
+    //     .reduce(function (acc, key) {
+    //       acc[key] = result[key];
+    //       return acc;
+    //     }, {});
+      
+    //   var cumulativeRevenue = 0;
+    //   for (const [key, value] of Object.entries(sortedResult)) {
+    //     console.log(`${key}: ${value}`);
+    //     cumulativeRevenue += value;
+    //     sortedResult[key] = cumulativeRevenue;
+    //   }
+    //   console.log(sortedResult);
+    //   return sortedResult;      
+    // },
   },
 
   components: {},
@@ -415,6 +590,10 @@ export default {
       this.filterEndDate = "";
       document.getElementById("filterForm").reset();
     },
+
+    foobarbarblacksheep() {
+      this.foo = !this.foo;
+    }
   },
 
   created() {
